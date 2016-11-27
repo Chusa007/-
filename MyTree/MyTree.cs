c4e39550bd37b4a9b31c1f8f9b5626b4314cfe94
Руслан Шамхалов <chusa007@mail.ru>
2016-11-27 21:21:32 +0300

    Дерево

---
+++ MyTree/MyTree.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyTree
+{
+    /// <summary>
+    /// Класс представляет элемент дерева.
+    /// </summary>
+    /// <typeparam name="TypeNode">Любой тип</typeparam>
+    public class TreeNode<TypeNode>
+    {
+        /// <summary>
+        /// Конструктор, инициализирующий элемент переданным значением.
+        /// </summary>
+        /// <param name="value">значение элемента</param>
+        public TreeNode(TypeNode value)
+        {
+            Value = value;
+            Children = new List<TreeNode<TypeNode>>();
+        }
+
+        /// <summary>
+        /// Коллекция потомков элемента.
+        /// </summary>
+        public List<TreeNode<TypeNode>> Children { get; internal set; }
+
+        /// <summary>
+        /// Представляет родительский элемент.
+        /// </summary>
+        public TreeNode<TypeNode> Parent { get; internal set; }
+
+        /// <summary>
+        /// Представляет значение элемента.
+        /// </summary>
+        public TypeNode Value { get; internal set; }
+
+    }
+
+    /// <summary>
+    /// Представляет дерево
+    /// </summary>
+    /// <typeparam name="T">Любой тип</typeparam>
+    public class MyTree<T>
+    {
+        /// <summary>
+        /// Корень дерева
+        /// </summary>
+        public TreeNode<T> Head { get; private set; } 
+
+        /// <summary>
+        ///Количество элементов дерева
+        /// </summary>
+        public int Length { get; private set; }
+
+        /// <summary>
+        /// Добавление нового элемента дерева
+        /// </summary>
+        /// <param name="value">значение элемента</param>
+        /// <param name="parent">родительский элемент или NULL, если добавляется корень дерева</param>
+        public void Add(TreeNode<T> parent, T value)
+        {
+            if (parent == null)
+            {
+                Head = new TreeNode<T>(value); //Если родитель отсутствует, то мы создаем новый корень дерева (новое дерево).
+                Length++;
+            }
+            else
+            {
+                if (Head == null) throw new InvalidOperationException("Дерево не имеет элементов"); //Если мы пытаемся в пустое дерево записать новый элемент с указанным (не пустым) родителем - выдаем ошибку.
+                TreeNode<T> newnode = new TreeNode<T>(value);
+                Length++;
+                newnode.Parent = parent;
+                parent.Children.Add(newnode);
+            }
+        }
+
+        /// <summary>
+        /// Удаление элемента
+        /// </summary>
+        /// <param name="removed_item">удаляемый элемент</param>
+        public void Remove(TreeNode<T> removed_item)
+        {
+            if (removed_item == null) throw new ArgumentNullException("Удаляемый элемент не может быть NULL");
+            if (removed_item.Parent == null) //Если удаляемый элемент - корень дерева.
+            {
+                Head = null;
+                for (int i = 0; i < removed_item.Children.Count; i++) //В цикле мы первого наследника удаляемого элемента ставим на место корня и перезаписываем наследников.
+                {
+                    if (i == 0)
+                    {
+                        Head = removed_item.Children[0];
+                        removed_item.Children[0].Parent = null;
+                    }
+                    else
+                    {
+                        removed_item.Children[i].Parent = Head;
+                        Head.Children.Add(removed_item.Children[i]);
+                    }
+                }
+            }
+            else //Если удаляемый элемент имеет родителя.
+            {
+                for (int i = 0; i < removed_item.Parent.Children.Count; i++) //Удаляем элемент из коллекции наследников родителя
+                {
+                    if (removed_item.Parent.Children[i].GetHashCode() == removed_item.GetHashCode()) removed_item.Parent.Children.RemoveAt(i);
+                }
+                TreeNode<T> newnode=null;
+                for (int i = 0; i < removed_item.Children.Count; i++) //В новый узел записываем первого наследника удаляемого элемента и добавляем к коллекции нового узла наследников удаляемого элемента.
+                {
+                    if (i == 0)
+                    {
+                        
+                        newnode = removed_item.Children[0];
+                        newnode.Parent = removed_item.Parent;
+                    }
+                    else
+                    {
+                        newnode.Children.Add(removed_item.Children[i]);
+                        removed_item.Children[i].Parent = newnode;
+                    }
+                }
+                if (newnode != null) removed_item.Parent.Children.Add(newnode); //Если удаляемый узел был не крайним в дереве - записываем его в коллекцию наследников родителя.
+            }
+        }
+
+        /// <summary>
+        /// Очистка дерева
+        /// </summary>
+        public void Clear()
+        {
+            Head = null;
+            Length = 0;
+        }
+
+    }
+}
---
+++ TreeViewerWPF/TreeViewerWPF/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using MyTree;
+
+namespace TreeViewerWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        public MyTree<string> Tree = new MyTree<string>();  //создания класса дерева
+        public TreeNode<string> trenode; //создания элемента дерева
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            menu.IsEnabled = true;
+        }
+
+        //Метод вызывающий диалоговое окно при нажатии на элемент дерева
+        public void NodeDialog(object sender, MouseButtonEventArgs e)
+        {
+            Button btn = sender as Button;
+            trenode = btn.Tag as TreeNode<string>;
+            NodeDialog DialogWindow = new NodeDialog();
+            DialogWindow.Owner = this;
+            DialogWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            DialogWindow.ShowDialog();
+            grid.Children.Clear();
+            if (Tree.Head != null) PrintTree<string>(Tree.Head, 0, 0);
+        }
+
+        //Метод отрисовки дерева при добавлении элемнта
+        public void PrintTree<T>(TreeNode<T> tree, double StartX, double StartY)
+        {
+            int number_d = 0;
+            Button tb = new Button();
+            tb.Content = Convert.ToString(tree.Value);
+            tb.Height = 30;
+            tb.Width = 50;
+            tb.Tag = tree;
+            tb.HorizontalAlignment = HorizontalAlignment.Left;
+            tb.VerticalAlignment = VerticalAlignment.Top;
+            tb.Margin = new Thickness(StartX, StartY, 0, 0);
+            tb.PreviewMouseLeftButtonDown += new MouseButtonEventHandler(this.NodeDialog);
+            grid.Children.Add(tb);
+            for (int i = 0; i < tree.Children.Count; i++)
+            {
+
+                if (i != 0)
+                {
+                    number_d += CountNodes(tree.Children[i - 1]);
+                    LineGeometry line = new LineGeometry();
+                    line.StartPoint = new Point((StartX + 25), (StartY + 30));
+                    line.EndPoint = new Point((StartX + 60), (StartY + 50 * (i + 1) + number_d * 50 + 15));
+                    System.Windows.Shapes.Path ptline = new System.Windows.Shapes.Path();
+                    ptline.Stroke = Brushes.Black;
+                    ptline.Data = line;
+                    grid.Children.Add(ptline);
+                    PrintTree<T>(tree.Children[i], StartX + 60, StartY + 50 * (i + 1) + number_d * 50);
+                }
+                else
+                {
+                    LineGeometry line = new LineGeometry();
+                    line.StartPoint = new Point((StartX + 25), (StartY + 30));
+                    line.EndPoint = new Point((StartX + 60), (StartY + 75));
+                    System.Windows.Shapes.Path ptline = new System.Windows.Shapes.Path();
+                    ptline.Stroke = Brushes.Black;
+                    ptline.Data = line;
+                    grid.Children.Add(ptline);
+                    PrintTree<T>(tree.Children[i], StartX + 60, StartY + 50);
+                }
+            }
+        }
+
+        private int CountNodes<T>(TreeNode<T> node)
+        {
+            int sum = 0;
+            foreach (var v in node.Children)
+            {
+                sum += CountNodes(v);
+            }
+            return sum += node.Children.Count;
+        }
+
+        public int WidthTree<T>(TreeNode<T> tree)
+        {
+            int count = tree.Children.Count;
+            List<int> list = new List<int>();
+            foreach (var v in tree.Children)
+            {
+                SearchWidthTree<T>(v, list, 1);
+            }
+            foreach (var v in list)
+            {
+                if (count < v) count = v;
+            }
+            return count;
+        }
+
+        private void SearchWidthTree<T>(TreeNode<T> tree, List<int> list, int level)
+        {
+            if (list.Count < level) list.Add(0);
+            list[level - 1] += tree.Children.Count;
+            foreach (var v in tree.Children)
+            {
+                SearchWidthTree<T>(v, list, level + 1);
+            }
+        }
+
+        
+        //Создания корня дерева
+        private void CreateNew_Click(object sender, RoutedEventArgs e)
+        {
+            trenode = null;
+            NodeDialog DialogWindow = new NodeDialog();
+            DialogWindow.Owner = this;
+            DialogWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            DialogWindow.ShowDialog();
+            grid.Children.Clear();
+            if (Tree.Head != null) PrintTree<string>(Tree.Head, 0, 0);
+        }
+        //Очистка формы от дерева
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Вы уверены, что хотите очистить Дерево?", "Очистка Дерева", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                Tree.Clear();
+                grid.Children.Clear();
+                trenode = null;
+            }
+        }
+    }
+}
